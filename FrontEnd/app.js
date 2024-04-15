33d3202a78778b5045e2b931bb0c4b46070e0e36
awasthilochan25@gmail.com <awasthilochan25@gmail.com>
2024-04-14 20:24:45 -0500

    inhertance

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -2,6 +2,7 @@
 class Person 
 {
     constructor(name,age){
+        console.log("person class constructor");
     this.name=name;
     this.age=age;
     }
@@ -13,3 +14,23 @@ class Person
 
 let p1 = new Person("lok",22);
 let p2 = new Person("los",22);
+
+
+class Person extends Person
+{
+    constructor(name,age,marks){
+    super(name,age)//parent class constructor being called
+this.marks=marks;    }
+
+   
+}
+
+
+class Teacher extends Person
+{
+    constructor(name,age,subjects){
+    super(name,age)//parent clas constructor us being called
+this.subjects=subjects;    }
+
+   
+}
